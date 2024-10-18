41eef6fe538e7f13f134290f9983dcb3f6dccf3f
Hector L Galarza <galarza.software@gmail.com>
2024-10-18 05:03:39 -0400

    Project creation

---
+++ client/src/main.cpp
@@ -0,0 +1,12 @@
+import irclibrary;
+#include <boost/asio.hpp>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cerr << "Not yet implemented\n";
+    // ... other server logic
+    return 0;
+    // ... other server logic
+}
---
+++ server/src/main.cpp
@@ -0,0 +1,10 @@
+import irclibrary;
+#include <boost/asio.hpp>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cerr << "Not yet implemented\n";
+    return 0;
+}
