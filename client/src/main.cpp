d00707dd140f05c960696eee9d2257f52c8218fa
Hector L Galarza <galarza.software@gmail.com>
2024-10-18 05:46:31 -0400

    Changed C++ version to c++23 and renamed project accordingly. Added support for cxxopts to manage the options of the applications.

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -1,10 +1,12 @@
 import irclibrary;
 #include <boost/asio.hpp>
 #include <iostream>
+#include <cxxopts.hpp>
 
 using namespace std;
 
 int main() {
+    cxxopts::Options options("IRC Client", "Basic IRC Client");
     cerr << "Not yet implemented\n";
     // ... other server logic
     return 0;
--- server/src/main.cpp
+++ server/src/main.cpp
@@ -1,10 +1,13 @@
 import irclibrary;
+
 #include <boost/asio.hpp>
 #include <iostream>
+#include <cxxopts.hpp>
 
 using namespace std;
 
 int main() {
+    cxxopts::Options options("IRC Server", "Basic IRC Server");
     cerr << "Not yet implemented\n";
     return 0;
 }
